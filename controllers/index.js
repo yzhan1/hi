9da82bc759df9ec3c26006fa390c3075754cb8bc
Yaoming Zhan <zhanyaoming2014@gmail.com>
2018-04-11 14:58:56 -0400

    'more token'

--- controllers/index.js
+++ controllers/index.js
@@ -14,8 +14,16 @@ class Controller {
     res.redirect(307, url);
   }
 
-  static getLoaderToken(req, res) {
+  static getLoadBalancerToken(req, res) {
     res.send('loaderio-dbccb84cd2f9c75a15a3c3e4479977d8');
+  }
+
+  static getReader1Token(req, res) {
+    res.send('loaderio-cb5b876def6d1805313fcb9b29ebd213');
+  }
+
+  static getReader2token(req, res) {
+    res.send('loaderio-6551bc7e87e4c846f9b7dcc62d4e21ea');
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router
   .post('*', controller.dispatchPost)
   .get('*', controller.dispatchGet)
-  .get('/loaderio-dbccb84cd2f9c75a15a3c3e4479977d8/', controller.getLoaderToken);
+  .get('/loaderio-dbccb84cd2f9c75a15a3c3e4479977d8/', controller.getLoadBalancerToken)
+  .get('/loaderio-cb5b876def6d1805313fcb9b29ebd213/', controller.getReader1Token)
+  .get('/loaderio-6551bc7e87e4c846f9b7dcc62d4e21ea/', controller.getReader2Token)
 
 module.exports = router;
