1eb89fc43749e8ab57c204f776600be7c41e0430
Yaoming Zhan <zhanyaoming2014@gmail.com>
2018-04-11 14:51:24 -0400

    'loader'

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router
   .post('*', controller.dispatchPost)
   .get('*', controller.dispatchGet)
-  .get('/loaderio-dbccb84cd2f9c75a15a3c3e4479977d8', controller.getLoaderToken);
+  .get('/loaderio-dbccb84cd2f9c75a15a3c3e4479977d8/', controller.getLoaderToken);
 
 module.exports = router;
